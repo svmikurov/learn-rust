3696f9c813401e1cbc862eb9a7e6ab52590e5c35
Sergei Mikurov <454004@mail.ru>
2025-10-18 21:00:25 +0500

    refactor(book/minigrep): Update args parse
    - Added constructor for `Config` creation
    - Added custom error handling for config creation

--- book/minigrep/src/main.rs
+++ book/minigrep/src/main.rs
@@ -1,10 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args)
+        .unwrap_or_else(
+            |err| {
+                println!("Problem parsing arguments: {err}");
+                process::exit(1);
+            }
+        );
 
     println!("Searching for \"{}\"", config.query);
     println!("In file '{}'", config.file_path);
@@ -22,15 +29,15 @@ struct Config {
     file_path: String,
 }
 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-fn parse_config(args: &[String]) -> Config {
-    // Делаем полную копию данных для экземпляра Config для владения,
-    // что занимает больше времени и памяти,
-    // чем сохранение ссылки на строку данных.
-    // Однако клонирование данных также делает код очень простым,
-    // потому что нам не нужно управлять временем жизни ссылок.
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    
-    Config { query, file_path }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        
+        Ok(Config { query, file_path })
+    }
 }
